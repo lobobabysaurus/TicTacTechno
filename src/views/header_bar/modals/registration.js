12795c03fe184c4c48197b664a6a9b41399b25b6
Phil Simmons <Phil.R.Simmons@gmail.com>
2016-02-14 13:51:53 -0500

    Added basics for form validation

--- src/views/header_bar/modals/registration.js
+++ src/views/header_bar/modals/registration.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Input } from 'react-bootstrap';
@@ -10,12 +11,85 @@ export default class RegistrationModal extends React.Component {
     super(props);
     this.state = {
       show: this.props.show,
-      closer: this.props.closer
+      closer: this.props.closer,
+      errors: {}
     };
   }
 
   componentWillReceiveProps = (props) => {
     this.setState({show: props.show});
+  }
+
+  registerUser = () => {
+    const registrationData = {
+      username: this.refs.username.getValue(),
+      password: this.refs.password.getValue(),
+      confirmPassword: this.refs.confirm_password.getValue(),
+      email: this.refs.email.getValue(),
+      confirmEmail: this.refs.confirm_email.getValue()
+    };
+    this.validateInput(registrationData);
+  }
+
+  validateInput = (data) => {
+    let errors = {};
+
+    errors = _.extend(errors, this.validateUsername(data));
+    errors = _.extend(errors, this.validatePassword(data));
+    errors = _.extend(errors, this.validateEmail(data));
+
+    this.setState({errors: errors});
+
+    return errors === {};
+  }
+
+  validateUsername = (data) => {
+    const errors = {};
+
+    if (!data.username){
+      errors.usernameError = this.error("Must provide a username");
+    }
+
+    return errors;
+  }
+
+  validatePassword = (data) => {
+    const errors = {};
+
+    if (!data.password){
+      errors.passwordError = this.error("Must provide a password");
+    }
+    else if (!data.confirmPassword) {
+      errors.confirmPasswordError = this.error("Must confirm password");
+    }
+    else if (data.password !== data.confirmPassword) {
+      errors.confirmPasswordError = this.error("Passwords do not match");
+    }
+
+    return errors;
+  }
+
+  validateEmail = (data) => {
+    const errors = {};
+
+    if (!data.email) {
+      errors.emailError = this.error("Must provide an email");
+    }
+    else if (!data.email.match(/[^@]+@[^@]+\.[^@]+/)) {
+      errors.emailError = this.error("Email is invalid");
+    }
+    else if (!data.confirmEmail) {
+      errors.confirmEmailError = this.error("Must confirm email");
+    }
+    else if (data.email !== data.confirmEmail) {
+      errors.confirmEmailError = this.error("Emails do not match");
+    }
+
+    return errors;
+  }
+
+  error(msg) {
+    return <small className='errorMessage'>{msg}</small>;
   }
 
   render() {
@@ -26,24 +100,34 @@ export default class RegistrationModal extends React.Component {
           <Modal.Title>Register with TicTacTechno</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          {this.state.errors.usernameError}
           <Input type='text'
+                 ref='username'
                  label='Username'
                  placeholder='Enter Username' />
+          {this.state.errors.passwordError}
           <Input type='password'
+                 ref='password'
                  label='Password'
                  placeholder='Enter Password'/>
+          {this.state.errors.confirmPasswordError}
           <Input type='password'
+                 ref='confirm_password'
                  label='Confirm Password'
                  placeholder='Confirm Password'/>
+          {this.state.errors.emailError}
           <Input type='email'
+                 ref='email'
                  label='Email Address'
                  placeholder='Enter Email Address' />
+          {this.state.errors.confirmEmailError}
           <Input type='email'
+                 ref='confirm_email'
                  label='Confirm Email'
                  placeholder = 'Confirm Email Address' />
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={this.state.closer}>Register</Button>
+          <Button onClick={this.registerUser}>Register</Button>
           <Button onClick={this.state.closer}>Close</Button>
         </Modal.Footer>
       </Modal>
