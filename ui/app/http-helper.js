2861424dd78c315aa7006cf8b2aa9e2f4ab8ef70
Phil Simmons <Phil.R.Simmons@gmail.com>
2016-02-28 01:04:14 -0500

    Added superagent-promise support

--- ui/app/http-helper.js
+++ ui/app/http-helper.js
@@ -1,21 +1,14 @@
 import request from 'superagent';
+import superagentPromisePlugin from 'superagent-promise-plugin';
 
 import { apiRoot } from 'config';
 
 export function post(resource, data) {
-  return new Promise((accept, reject) => {
-    request
-      .post(`${apiRoot}${resource}`)
-      .set('Content-Type', 'application/json')
-      .accept('application/json')
-      .send(data)
-      .end((error, response) => {
-        if(error) {
-          reject(error);
-        }
-        else {
-          accept(response);
-        }
-      });
-  });
+  return request
+          .post(`${apiRoot}${resource}`)
+          .use(superagentPromisePlugin)
+          .set('Content-Type', 'application/json')
+          .accept('application/json')
+          .send(data)
+          .end();
 }
